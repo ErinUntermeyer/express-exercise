ae6d47eae77538325e3deec3f977934b3d01f802
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-03 11:42:10 -0600

    Add title and food data

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const app = express()
 app.use(express.json())
 
 app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Express Exercise - Food Edition'
+app.locals.food = [
+	{ id: 1, type: 'pizza', meal: ['breakfast', 'lunch', 'dinner'] },
+	{ id: 2, type: 'pasta', meal: ['lunch', 'dinner'] },
+	{ id: 3, type: 'cereal', meal: ['breakfast'] }
+]
